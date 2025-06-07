a57d64b07bc94b5b5c67c7ec3bab3078bca91731
VulcaN <127948316+pimondontknow@users.noreply.github.com>
2025-06-07 15:59:47 +0800

    fix some bugs

--- app/src/main/java/com/juanantbuit/weatherproject/di/NetworkModule.kt
+++ app/src/main/java/com/juanantbuit/weatherproject/di/NetworkModule.kt
@@ -6,10 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -19,16 +17,8 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideRetrofit():Retrofit {
-        val okHttpClient = OkHttpClient.Builder()
-            .connectTimeout(120, TimeUnit.SECONDS)
-            .readTimeout(120, TimeUnit.SECONDS)
-            .writeTimeout(120, TimeUnit.SECONDS)
-            .build()
-
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+        return Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
             .build()
     }
 
